335623abd916ddc0b6e6a73567e593889274de9d
hapeeeeee <623151737@qq.com>
2025-07-01 15:44:47 +0800

    change build.sh for ingore warning

--- wasm-compile.sh
+++ wasm-compile.sh
@@ -1,4 +1,6 @@
-# wasm-pack build --target web --out-dir lsp-pkg -- . -Z build-std=std,panic_abort
-
-cargo build -Zbuild-std=std,panic_abort --target=wasm32-wasip1-threads --release 
+export RUSTFLAGS="-A warnings" && \
+cargo build \
+    -Zbuild-std=std,panic_abort \
+    --target=wasm32-wasip1-threads \
+    --release
 cp -f /data/zhangxiao/move/sui-move-analyzer/target/wasm32-wasip1-threads/release/sui_move_analyzer2.wasm /data/zhangxiao/move/web-ide/move-web-ide-bak/webIDE/public
